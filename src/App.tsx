5782b083155941f879e3d56c6b129b7118483f85
antonorlov <fogelo@yandex.ru>
2022-05-26 01:09:40 +0300

    prepare to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Provider} from "react-redux";
 import './App.css';
+import store from "./s-1-main/m-2-bll/store";
+import Main from "./s-1-main/m-1-ui/Main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// add context
+const App: React.FC = () => {
+    return (
+        <div className="App">
+                <Provider store={store}>
+                    <Main/>
+                </Provider>
+        </div>
+    );
+};
 
 export default App;
